abcc9e22ad1faef7c8382defc0b232c39f43bf08
The Tempest76 <afnaanayub26@gmail.com>
2025-12-02 01:24:52 +0530

    advent_of_code2025

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+# making a circular queeue to store numbers from 1 to 99 and move the pointer to the starting if it hits 99 and count the number of times it hits 0
+def import_directions(file_path):
+    with open(file_path, 'r') as file:
+        directions = [int(line.strip().replace("L", "").replace("R", "")) for line in file.readlines()]
+    return directions
+
+def count_circular_queue_hits(directions):
+    queue = list(range(100))
+    pointer = 0
+    hit_count = 0
+
+    for move in directions:
+        pointer = (pointer + move) % len(queue)  
+        if queue[pointer] == 0:
+            hit_count += 1
+
+    return hit_count
+if __name__ == "__main__":
+    directions = import_directions("day1/input.txt")
+    hits = count_circular_queue_hits(directions)
+    print(f"The pointer hit 0 a total of {hits} times.")
